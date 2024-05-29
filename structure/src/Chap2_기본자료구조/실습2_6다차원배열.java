49bae8ef807f212fce153b9f199af979c2f2dad0
Park-Dongheon <cdsded@naver.com>
2024-05-29 17:58:38 +0900

    structure_day2

--- structure/src/Chap2_기본자료구조/실습2_6다차원배열.java
+++ structure/src/Chap2_기본자료구조/실습2_6다차원배열.java
@@ -14,7 +14,8 @@ public class 실습2_6다차원배열 {
 		double [][]B = new double[3][4];
 		double [][]C = new double[2][4];
 
-		inputData(A);inputData(B);
+		inputData(A);
+		inputData(B);
 		double [][]D = A.clone();//교재83 - 배열 복제
 	
 		showData("A[2][3] = ",A);
@@ -38,32 +39,60 @@ public class 실습2_6다차원배열 {
 		
 		System.out.println("F = " + Arrays.deepToString(F));//2차원 배열 처리 
 	}
-	inputData() {//double 난수 0.0 ~ 1.0 생성
-
-
-	}
-	static void showData(String msg, int[][]data) {//주어진 문자열을 출력하고 배열을 2차원 형태로 출력
-		System.out.println(msg);
-		for() {
-			
+	
+	static void inputData(double[][] matrix) {//double 난수 0.0 ~ 1.0 생성
+		Random rnd = new Random();
+		
+		for(int i = 0; i < matrix.length; i++) {
+			for(int j = 0; j < matrix[0].length; j++) {
+				matrix[i][j] = rnd.nextDouble() * 1.0;
+			}
 		}
 	}
-	equals() {//두 행렬의 사이즈가 같고 값도 모두 같아야 행렬 2개는 equals을 true로 리턴
-
-
+	
+	static void showData(String msg, double[][]data) {//주어진 문자열을 출력하고 배열을 2차원 형태로 출력
+		System.out.println(msg);
+		for(double[] row : data) {
+			for(double col : row) {
+				System.out.printf("%.2f ",col);
+			}
+			System.out.println();
+		}
+		
+		
 	}
-	static double[][] addMatrix(double[][]A, double[][]B) {//행렬 덧셈 결과를 리턴
+	
+	static boolean equals(double[][] a, double[][] d) {//두 행렬의 사이즈가 같고 값도 모두 같아야 행렬 2개는 equals을 true로 리턴
+		return false;
+	}
+	
+	static double[][] addMatrix(double[][]A, double[][]D) {//행렬 덧셈 결과를 리턴
 		double[][] C = new double[2][3];
 		// 코딩
+		for(int i = 0; i < A.length; i++) {
+			for(int j = 0; j < A[0].length; j++) {
+				C[i][j] = A[i][j] + D[i][j];
+			}
+		}
+		
 		return C;
 	}
-	multiplyMatrix() {//행렬 곱셈 결과를 리턴
-
+	
+	static double[][] multiplyMatrix(double[][] A, double[][] B) {
+		double[][] C = new double[2][4];
+		for(int i = 0; i < A.length; i++ ) {
+			for(int j = 0; j < A[0].length; j++) {
+				for(int )
+			}
+		}
 		
+		
+		return C;//행렬 곱셈 결과를 리턴
 	}
-	transposeMatrix() {//전치 행렬을 리턴
-
+	
+	static double[][] transposeMatrix(double[][] matrix) {//전치 행렬을 리턴
 		
+		return matrix;
 	}
 }
 
